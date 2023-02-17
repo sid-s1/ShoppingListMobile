db84970bbd61ffae814ddd0dc5d69010b3af5ad0
Siddharth Singh <siddharth.s1@outlook.com>
2023-02-17 17:54:02 +1100

    Creating rows using TouchableOpacity component; created a separate header file; linked vector icons after some debugging

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
 
 const App = () => {
+  const [items, setItems] = useState([
+    { id: 1, text: 'Milk' },
+    { id: 2, text: 'Eggs' },
+    { id: 3, text: 'Bread' },
+    { id: 4, text: 'Juice' }
+  ]);
+
   return (
-    <View>
-      <Text>
-        Hello!
-      </Text>
+    <View style={styles.container}>
+      <Header title='Shopping List' />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <ListItem item={item} />}
+      />
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60
+  }
+});
 
 export default App;
